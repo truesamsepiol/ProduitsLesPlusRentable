b1fbeb4373d6e3731030493873617b6fee1b3d8a
truesamsepiol <true.samsepiol@gmail.com>
2023-08-31 12:34:42 +0100

    Version 1.1, ajout benefice en terme de pourcentage Benefice / prix d'achat

--- script.py
+++ script.py
@@ -10,13 +10,26 @@ def usage():
     print("\nUsage:\n\tpython3 " + sys.argv[0] + " <cheminVersFichier>")
     print("\n\tLe fichier doit etre au format CSV")
 
+def generationDiagramme(nomFichier, tailleX, tailleY, titre, labelX, valeursX, valeursY):
+
+    nomFichierPdf = nomFichier + ".pdf"
+    nomFichierPng = nomFichier + ".png"
+    # Generation d'un diagramme en baton
+    plt.figure(figsize=(tailleX, tailleY)) 
+    plt.grid(axis = "x", color = "black", linestyle="--")
+    plt.title(titre)
+    plt.xlabel(labelX)
+    plt.barh(valeursX, valeursY) # baton horizontal
+    #plt.bar(valeursX, valeursY) # baton verticale
+    plt.savefig(nomFichierPdf)
+    plt.savefig(nomFichierPng)
+
 def traitement(fichier, numero):
     print("Debut de traitement du fichier")
     
     designations = [] 
-    benefices = []
-    nomFichierSortieImage = "RentabiliteDesProduits." + str(numero) + ".png"
-    nomFichierSortiePdf = "RentabiliteDesProduits." + str(numero) + ".pdf"
+    benefices    = []
+    pourcentages = []
     donnees = csv.reader(fichier)
     for ligne in donnees:
         prixDeVente    = int(ligne[3])
@@ -24,19 +37,15 @@ def traitement(fichier, numero):
         prixDachat     = int(ligne[1])
         # Benefice par casier vendu dans le cadre d'une buvette
         benefice       = prixDeVente * quantiteAchete - prixDachat 
-        print(ligne[0]  + ": " + str(benefice))
+        # Pourcentage par casier vendu 
+        pourcentage    = 100*(float(benefice) / float(prixDachat))
+        print(ligne[0]  + ": " + str(benefice) + " " + str(pourcentage))
         designations.append(ligne[0])
         benefices.append(benefice)
+        pourcentages.append(pourcentage)
 
-    # Generation d'un diagramme en baton
-    plt.figure(figsize=(10, 13)) # baton vertical
-    plt.grid(axis = "x", color = "black", linestyle="--")
-    plt.title("Bénéfices par casier vendu")
-    plt.xlabel("Bénéfices en FCFA")
-    plt.barh(designations, benefices)
-    #plt.bar(designations, benefices) # baton horizontal
-    plt.savefig(nomFichierSortieImage)
-    plt.savefig(nomFichierSortiePdf)
+    generationDiagramme("beneficeParCasier", 10, 30, "Benefice par casier vendu", "Benefice en FCFA", designations, benefices)
+    generationDiagramme("pourcentageBeneficeParCasier", 10, 30, "Benefice en pourcentage par casier vendu", "Benefice en %", designations, pourcentages)
     print("Fin de traitement du fichier\n\n")
 
 if __name__ == "__main__":
